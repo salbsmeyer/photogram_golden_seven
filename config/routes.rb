49dc7237b1d314ce198451d71ebf012b10caa03c
salbsmeyer <sophiealbsmeyer@Sophies-MacBook-Pro.local>
2015-08-01 11:19:03 -0500

    Save1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #CREATE new form
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("photo/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+#DELETE
+get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+#UPDATE
+get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
 end
